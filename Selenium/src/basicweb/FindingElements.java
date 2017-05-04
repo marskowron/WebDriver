64915fb08793f2c3290fed464d1d4f7d46abde1f
marskowron <marcinskowron@o2.pl>
2017-05-04 20:24:01 +0200

    finding element

---
+++ Selenium/src/basicweb/FindingElements.java
@@ -0,0 +1,24 @@
+package basicweb;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class FindingElements {
+
+	public static void main(String[] args) {
+	
+		WebDriver driver = new FirefoxDriver();
+		String baseURL = "http://google.com";
+		driver.get(baseURL);
+		driver.manage().window().maximize(); // maksymalizuje okno
+		
+		driver.findElement(By.id("lst-ib")).sendKeys("letskodeit"); //wyszukuje element po Id - wpisuje tekst
+		driver.findElement(By.xpath("//*[@id='_fZl']")).click(); //wyszukuje element po xpathie i klika
+		driver.findElement(By.name("btnG")).click(); //wyszukuje element po nazwie i klika
+		
+		//driver.close();
+
+	}
+
+}
