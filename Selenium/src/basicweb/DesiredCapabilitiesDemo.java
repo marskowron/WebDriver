b716855b99397d556d62f3647c4d20a5d4a28204
marskowron <marcinskowron@o2.pl>
2017-05-04 10:40:50 +0200

    name fix

--- Selenium/src/basicweb/DesiredCapabilitiesDemo.java
+++ Selenium/src/basicweb/DesiredCapabilitiesDemo.java
@@ -5,15 +5,15 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-//import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
-public class DesiredCapabilities {
+public class DesiredCapabilitiesDemo {
 	
 	public static void main(String[] args) throws Exception {
 		WebDriver driver;
 		String baseURL = "https://letskodeit.teachable.com/pages/practice";
 		System.setProperty("webdriver.gecko.driver", "E:\\workspace\\selenium-java-3.4.0\\geckodriver.exe");  
-		org.openqa.selenium.remote.DesiredCapabilities caps = org.openqa.selenium.remote.DesiredCapabilities.firefox();
+		DesiredCapabilities caps = DesiredCapabilities.firefox();
 		caps.setBrowserName("firefox");
 		caps.setPlatform(Platform.WINDOWS);
 		
